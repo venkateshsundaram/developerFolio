ed9204f2bfbe15c0c43c8fb8cb7c32a803b58c06
venkateshsundaram <venkateshsundaram@users.noreply.github.com>
2021-03-20 08:54:35 +0000

    Deploying to gh-pages from  @ 07c92f3993856a52cd34444f28c88ec1200a668a 🚀

---
+++ precache-manifest.576c12fa47f401398c196e56d7d5fb72.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "894ba03a02eb9b4068cb5d7847047267",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25baf3f11ce019adbfba",
+    "url": "/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "f84e24b9b83c2860dd46",
+    "url": "/static/css/4.3e7d09e6.chunk.css"
+  },
+  {
+    "revision": "42d91e697520cc8130f2",
+    "url": "/static/css/main.207db4cf.chunk.css"
+  },
+  {
+    "revision": "8537a874d293dfde2e16",
+    "url": "/static/js/2.a8a8d463.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.a8a8d463.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25baf3f11ce019adbfba",
+    "url": "/static/js/3.22c00cb9.chunk.js"
+  },
+  {
+    "revision": "f84e24b9b83c2860dd46",
+    "url": "/static/js/4.8c70f7a5.chunk.js"
+  },
+  {
+    "revision": "42d91e697520cc8130f2",
+    "url": "/static/js/main.7bb4e04e.chunk.js"
+  },
+  {
+    "revision": "1b87af98615767255950",
+    "url": "/static/js/runtime-main.a2440bee.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "130a261e546f31e119af183d5f8c36f5",
+    "url": "/static/media/alineLogo.130a261e.png"
+  },
+  {
+    "revision": "0c169fcea89f8e7d99f56290be9b4bde",
+    "url": "/static/media/colanInfotechLogo.0c169fce.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "4069429bfb2a359f6b7627109eaa1963",
+    "url": "/static/media/freshworks.4069429b.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "1ea1cf0863ec76bca08079647e9aab90",
+    "url": "/static/media/vickramCollegeLogo.1ea1cf08.jpeg"
+  }
+]);
